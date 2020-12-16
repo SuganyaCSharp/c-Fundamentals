93df9e2a828e868fe4bb99c81ef2f69681b804e1
BC226H2-OOBE-01\venkat <suganyapselvam@gmail.com>
2020-12-16 11:35:38 +0530

    i Wrote VAluetype and Referncetype

--- CSharpFundamentals/ValuetypeAndReferenceType/Program.cs
+++ CSharpFundamentals/ValuetypeAndReferenceType/Program.cs
@@ -6,7 +6,80 @@ namespace ValuetypeAndReferenceType
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RefTest2();
+            //RefTest();
+            //ValueTest();
+            //OutUseCase();
+        }
+        private static void OutUseCase()
+        {
+            int q;
+            FindRemAndQuo(123, 7, out int rem, out q);
+            Console.WriteLine(rem);
+            Console.WriteLine(q);
+        }
+        private static void FindRemAndQuo(int a, int b, out int rem, out int q)
+        {
+            rem = a % b;
+            q = a / b;
+        }
+        private static void ValueTest()
+        {
+            int X = 10;
+            changeValue(X);
+            Console.WriteLine(X);
+            changeValueRef(ref X);
+            Console.WriteLine(X);
+            changeValueOut(out X);
+            Console.WriteLine(X);
+        }
+        private static void RefTest2()
+        {
+            var refernce = new ReferenceType("sugu", 22);
+            Console.WriteLine(refernce.ToString());
+            PropertyChange(refernce);
+            Console.WriteLine(refernce.ToString());
+        }
+        private static void RefTest()
+        {
+            ReferenceType referencetype = new ReferenceType("sugu", 22);
+            Console.WriteLine(referencetype.ToString());
+            GetReference(referencetype);
+            Console.WriteLine(referencetype.ToString());
+            GetReferenceRef(ref referencetype);
+            Console.WriteLine(referencetype.ToString());
+            GetReferenceOut(out referencetype);
+            Console.WriteLine(referencetype.ToString());
+        }
+        static void PropertyChange(ReferenceType referenceType)
+        {
+            referenceType.Name = "sagu";
+            referenceType.Age = 55
+;        }
+        static void GetReference(ReferenceType referenceType)
+        {
+            referenceType = new ReferenceType("venkat",23);
+        }
+        static void GetReferenceRef(ref ReferenceType referenceType)
+        {
+            referenceType = new ReferenceType("venkat", 23);
+        }
+        static void GetReferenceOut(out ReferenceType referenceType)
+        {
+            referenceType = new ReferenceType("venkat", 23);
+        }
+
+        static void changeValue(int x)
+        {
+            x = 20;
+        }
+        static void changeValueRef(ref int x)
+        {
+            x = 20;
+        }
+        static void changeValueOut(out int x)
+        {
+            x = 20;
         }
     }
 }
---
+++ CSharpFundamentals/ValuetypeAndReferenceType/ReferenceType.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValuetypeAndReferenceType
+{
+    class ReferenceType
+    {
+        public string  Name {get; set; }
+        public int Age { get; set; }
+        public ReferenceType(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name},Age: {Age}";     
+        }
+    }
+}
